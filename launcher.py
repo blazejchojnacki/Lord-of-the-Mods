f7d05576135ed5504ac789bc69fa752724e5b424
blazejchojnacki <blazejchojnacki@outlook.com>
2025-12-22 17:01:33 +0100

    Moved Python modules into /source/. Added creating snapshot folders in initiator.py

---
+++ launcher.py
@@ -0,0 +1,7 @@
+"""
+Core of the 'Lord of the Mods.exe' to launch the application composed of the modules under /source/
+Alternatively, the modules can be tested via trials.py
+"""
+
+if __name__ == "__main__":
+    import source.window_console
