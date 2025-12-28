4e3ee1d963517d230ad81c2bca40c3755e135c81
blazejchojnacki <blazejchojnacki@outlook.com>
2025-12-28 18:58:39 +0100

    in file_editor.py added text_find_multiple, find_objects_in_str_file, link_check. Renamed functions.

--- source/file_editor.py
+++ source/file_editor.py
@@ -3,25 +3,13 @@ import os
 import shutil
 import re
 
-# from source.file_interpreter import load_items, load_items_part, print_items, print_items_part, comment_out, \
-#     recognize_item_class
 import source.constructor as c
 import source.shared as s
 
-# TODO later: reference checker
 # TODO later: automated proposition of #include creation or child adopting
 
 
-def log(output):
-    if os.path.isfile(f'{s.LOG_PATH}/file_changes.txt'):
-        with open(f'{s.LOG_PATH}/file_changes.txt', 'a') as log_file:
-            log_file.write(output + '\n')
-    else:
-        with open(f'{s.LOG_PATH}/file_changes.txt', 'w') as log_file:
-            log_file.write(output + '\n')
-
-
-def convert_string(string, direction='automatic'):
+def reformat_string(string, direction='automatic'):
     """
     converts the \n \t \r characters for reading or for finding the string in a file
     :param string: str to convert
@@ -47,28 +35,103 @@ def convert_string(string, direction='automatic'):
             return string
 
 
-def find_replace_text(find, replace_with=None, scope='', exceptions=None, mode='initiate'):
+def text_find_multiple(find_list: list, scope='', exceptions=None, not_used=None, output_file='output.txt'):
+    """
+     finds multiple given strings in a given file or folder of files
+     :param find_list: list of strings to find
+     :param scope: path to file or folder where to look for the strings
+     :param exceptions: list of files to omit
+     :param not_used: recursive value placeholder
+     :param output_file:
+     """
+    if exceptions is None:
+        exceptions = []
+    if not_used is None:
+        not_used = set(find_list)
+    output = ''
+    for exception in exceptions:
+        if scope.replace('\\', '/') == exception.replace('\\', '/'):
+            return not_used
+    if os.path.isfile(scope):
+        try:
+            with open(scope) as file_buffer:
+                file_content = file_buffer.read()
+            output += f'In {scope}\n'
+            for find in find_list:
+                find_count = file_content.count(find)
+                if find_count > 0:
+                    find_dict = {}
+                    index_line = 1
+                    content_index_prev = 0
+                    for content_part in file_content.split(find)[:-1]:
+                        index_line += content_part.count('\n')
+                        content_index = (
+                                file_content.index(content_part, content_index_prev) + len(content_part) + len(find))
+                        if file_content[content_index] in [' ', '\n', '\t', ';', '/']:
+                            content_index_prev = content_index
+                            if find in not_used:
+                                not_used.remove(find)
+                            text_line = file_content.split('\n')[index_line - 1]
+                            find_dict[index_line] = text_line
+                        else:
+                            find_count -= 1
+                    if find_dict:
+                        output += f'\tref: {find} found {find_count}:\n'
+                        for key in find_dict:
+                            output += f'\t\tin line {key} "{find_dict[key]}"\n'
+            if output and output_file:
+                s.log(output, file=output_file)
+        except UnicodeDecodeError:
+            raise s.InternalWarning(f'file {scope} unreadable')
+    elif os.path.isdir(scope):
+        file_paths = os.listdir(scope)
+        for file_path in file_paths:
+            not_used = not_used.intersection(
+                text_find_multiple(find_list, f'{scope}/{file_path}', exceptions, not_used, output_file))
+            continue
+    return not_used
+
+
+def find_objects_in_str_file(file, scope, exceptions, used_file='reference_RotWK_lotr_str.txt',
+                             unused_file='reference_RotWK_lotr_str_unused.txt'):
+    items = c.ConstructFile(file)
+    if file.endswith('.str'):
+        titles_list = []
+        for item in items:
+            titles_list.append(f"{item[1]['class']}:{item[1]['name']}")
+        output_set = text_find_multiple(titles_list, scope=scope, exceptions=exceptions, output_file=used_file)
+        output_ordered = list(output_set)
+        output_ordered.sort()
+        output = 'Unused references:\n'
+        for not_used_ref in output_ordered:
+            output += f'{not_used_ref}\n'
+        s.log(output, file=unused_file)
+    elif file.endswith('.ini'):
+        pass
+
+
+def text_find_replace(find, replace_with=None, scope='', exceptions=None, mode='initiate'):
     """ replaces a given string by another in a given file or folder of files """
     output = ''
     if not find:
-        return 'file_editor.replace_text() aborted - empty string to find'
+        return f'{s.this_module()}.{s.this_object()} aborted - empty string to find'
     if 'initiate' in mode:
         output += f'{datetime.now()}'
         if replace_with is not None:
-            output += f' command: replace "{convert_string(find, direction="display")}"\n'
-            output += f'\twith "{convert_string(replace_with, direction="display")}"\n\tin {scope}.\n'
+            output += f' command: replace "{reformat_string(find, direction="display")}"\n'
+            output += f'\twith "{reformat_string(replace_with, direction="display")}"\n\tin {scope}.\n'
         else:
-            output += f' command: find "{convert_string(find, direction="display")}"\n'
+            output += f' command: find "{reformat_string(find, direction="display")}"\n'
             output += f' in {scope}. \n'
         if exceptions:
             output += f'\texcept in {str(exceptions).strip("[]")}\n'
         output += 'result: '
-    find = convert_string(find, direction='process')
+    find = reformat_string(find, direction='process')
     if replace_with is not None:
-        replace_with = convert_string(replace_with, direction='process')
+        replace_with = reformat_string(replace_with, direction='process')
     if ', ' in scope and 'initiate' in mode:
         for scope_element in scope.split(', '):
-            output += find_replace_text(find, replace_with, scope_element, exceptions, mode='part')
+            output += text_find_replace(find, replace_with, scope_element, exceptions, mode='part')
         return output
     if exceptions:
         if scope in exceptions:
@@ -76,16 +139,16 @@ def find_replace_text(find, replace_with=None, scope='', exceptions=None, mode='
     if os.path.isfile(scope):
         try:
             file_content = c.load_file(scope)
-            if file_content.lower().count(find.lower()) > 0:
+            if file_content.casefold().count(find.casefold()) > 0:
                 if 'include' in mode:
                     output = file_content[
-                             file_content.rfind('#include', 0, file_content.lower().find(find.lower())):
-                             file_content.find('\n', file_content.lower().find(find.lower()))]
+                             file_content.rfind('#include', 0, file_content.casefold().find(find.casefold())):
+                             file_content.find('\n', file_content.casefold().find(find.casefold()))]
                 elif replace_with is not None:
                     new_file_content = ''
-                    output += f'\t{file_content.lower().count(find.lower())} replaced in {scope}\n'
+                    output += f'\t{file_content.casefold().count(find.casefold())} replaced in {scope}\n'
                     index_line = 1
-                    content_parts = file_content.lower().split(find.lower())
+                    content_parts = file_content.casefold().split(find.casefold())
                     for content_part in content_parts:
                         index_line += content_part.count('\n')
                         text_line = file_content.split('\n')[index_line - 1]
@@ -96,33 +159,33 @@ def find_replace_text(find, replace_with=None, scope='', exceptions=None, mode='
                     with open(scope, 'w') as file:
                         file.write(new_file_content)
                 else:
-                    output += f'\tin {scope} found {file_content.lower().count(find.lower())}:\n'
+                    output += f'\tin {scope} found {file_content.casefold().count(find.casefold())}:\n'
                     index_line = 1
-                    for content_part in file_content.lower().split(find.lower())[:-1]:
+                    for content_part in file_content.casefold().split(find.casefold())[:-1]:
                         index_line += content_part.count('\n')
                         text_line = file_content.split('\n')[index_line - 1]
                         output += f'\t\tin line {index_line} "{text_line}"\n'
             elif 'initiate' in mode:
                 output += f'\tfound none\n'
         except UnicodeDecodeError:
-            print(f'file_editor.replace_text() error: file {scope} unreadable')
+            raise s.InternalError(f"file {scope} unreadable")
     elif os.path.isdir(scope):
         file_paths = os.listdir(scope)
         for file_path in file_paths:
-            output += find_replace_text(find, replace_with, f'{scope}/{file_path}', exceptions, mode='part')  # mode=0
+            output += text_find_replace(find, replace_with, f'{scope}/{file_path}', exceptions, mode='part')  # overwrite=0
     if 'initiate' in mode:
-        log(output)
+        s.log(output)
     return output
 
 
-def update_reference(new_path, in_file_or_folder, inc_file=None, mode=0):
+def update_links_to_inc(new_path, in_file_or_folder, inc_file=None, overwrite=True):
     """
     internal function triggered when a .inc file is moved.
      needs to be a separate function to call itself without triggering the rest of the move function
     :param new_path:
     :param in_file_or_folder:
     :param inc_file:
-    :param mode:
+    :param overwrite:
     :return: logs of updated #include paths
     """
     file_exceptions = [r"O:\Lord of the Mods\_LIBRARY\AotR-override\AOTR8\aotr\data\ini\eva.ini".replace('\\', '/'),
@@ -137,22 +200,23 @@ def update_reference(new_path, in_file_or_folder, inc_file=None, mode=0):
         for folder_path in folders_paths:
             if folder_path in folder_exceptions:
                 continue
-            output += update_reference(new_path=new_path, in_file_or_folder=f'{in_file_or_folder}/{folder_path}', inc_file=inc_file)
+            output += update_links_to_inc(
+                new_path=new_path, in_file_or_folder=f'{in_file_or_folder}/{folder_path}', inc_file=inc_file)
     elif os.path.isfile(in_file_or_folder) and in_file_or_folder.endswith('.ini'):
         if in_file_or_folder in file_exceptions:
             return ''
-        line_include += find_replace_text(find=new_path.split('/')[-1], scope=in_file_or_folder, mode='include')
+        line_include += text_find_replace(find=new_path.split('/')[-1], scope=in_file_or_folder, mode='include')
         if line_include:
             old_path = in_file_or_folder
     if old_path:
         output += f'in file {in_file_or_folder}:\n'
-        with open(in_file_or_folder) as file_checked:
-            lines = file_checked.readlines()
+        with open(in_file_or_folder) as file_stream:
+            lines = file_stream.readlines()
         new_content = ''
         line_counter = 0
         for line in lines:
             line_counter += 1
-            if "#include" in line and line.strip()[0] not in s.INI_COMMENTS and inc_file.lower() in line.lower():
+            if "#include" in line and line.strip()[0] not in s.INI_COMMENTS and inc_file.casefold() in line.casefold():
                 path_old_include, path_new_include = '', ''
                 if line_include in line:
                     path_old_include = line_include.strip()[len('#include "'):line_include.strip().rfind('"')]
@@ -171,13 +235,13 @@ def update_reference(new_path, in_file_or_folder, inc_file=None, mode=0):
                     output += f'\tin line {line_counter} #include "{path_old_include}" left unchanged.\n'
             else:
                 new_content += line
-        if ''.join(lines) != new_content and mode == 0:
+        if ''.join(lines) != new_content and overwrite is True:
             with open(in_file_or_folder, 'w') as file_overwritten:
                 file_overwritten.write(new_content)
     return output
 
 
-def update_single_reference(old_path, new_path, mode=0):
+def update_links_in_ini(old_path, new_path, mode=0):
     """
     Internal function triggered when a .ini file is moved
     :param old_path:
@@ -219,9 +283,6 @@ def move_file(full_path, to_folder, mode=0):
     output = ''
     file_name = full_path.replace('\\', '/').split('/')[-1]
     to_folder = to_folder.replace('\\', '/')
-    if s.LIBRARY.replace('\\', '/') not in to_folder:
-        # raise InternalError('file_editor.move_file aborted - destination path not in MODS_FOLDER')
-        pass
     try:
         if mode == 0:
             output += f'{datetime.now()}'
@@ -229,12 +290,13 @@ def move_file(full_path, to_folder, mode=0):
             shutil.move(full_path, f'{to_folder}/{file_name}')
         if file_name.endswith('.inc'):
             ini_folder = to_folder[:to_folder.find(s.INI_PATH_PART) + len(s.INI_PATH_PART)]
-            output += update_reference(new_path=f'{to_folder}/{file_name}', in_file_or_folder=ini_folder, inc_file=file_name)
+            output += update_links_to_inc(
+                new_path=f'{to_folder}/{file_name}', in_file_or_folder=ini_folder, inc_file=file_name)
         elif file_name.endswith('.ini'):
-            output += update_single_reference(old_path=full_path, new_path=f'{to_folder}/{file_name}', mode=mode)
+            output += update_links_in_ini(old_path=full_path, new_path=f'{to_folder}/{file_name}', mode=mode)
     except shutil.Error:
         raise s.InternalError('file_editor.move_file error: erroneous path')
-    log(output)
+    s.log(output)
     return output
 
 
@@ -296,29 +358,45 @@ def duplicates_find(of_object_or_file, in_file_or_directory=None):
     return output
 
 
-def load_directories(full_path, mode=0):
-    """
+no_reference_params = ['KindOf']
+no_reference_values = ['Yes', 'No', 'None', 'NONE', 'ALL']
+reference_value_dict = {
+    'OBJECT:': '/data/lotr.str'
+}
 
-    :param full_path:
-    :param mode: mode=0 makes the function omit the full path,
-     mode=1 makes the function provide the full path of each item
-    :return: a tuple of two lists of folders and files contained in the given directory
-    """
-    output_folders = []
-    output_files = []
-    try:
-        items = os.listdir(full_path)
-    except PermissionError as error:
-        raise s.InternalError(error.strerror)
-    for item in items:
-        if os.path.isdir(f'{full_path}/{item}'):
-            output_folders.append(f'{(full_path + "/") * mode}{item}')
-            if mode == 1:
-                add_folders, add_files = load_directories(output_folders[-1], mode=1)
-                if add_folders:
-                    output_folders.append(add_folders)
-                if add_files:
-                    output_files.append(add_files)
-        elif os.path.isfile(f'{full_path}/{item}'):
-            output_files.append(f'{(full_path + "/") * mode}{item}')
-    return output_folders, output_files
+
+def link_check(construct):
+    if isinstance(construct, str):
+        if os.path.isfile(construct):
+            construct = c.ConstructFile(construct)
+            link_check(construct)
+        else:
+            raise s.InternalError(f"invalid path {construct}")
+    for element in construct:
+        if isinstance(element, c.ConstructLevel):
+            link_check(element)
+        elif isinstance(element, dict):
+            if 'statement' in element:
+                # if element['statement'].strip() not in s.INI_ENDS:
+                if ' = ' in element['statement'].strip():
+                    param, value = element['statement'].strip().split(' = ')
+                    if value.isnumeric() or value in no_reference_values:
+                        pass
+                    elif param in no_reference_params:
+                        pass
+                    else:
+                        for ref_value_key in reference_value_dict:
+                            if value.startswith(ref_value_key):
+                                reference = value[len(ref_value_key):]
+                                print(f'ref: {reference}')
+                                break
+                else:
+                    print(f'line : {element['statement'].strip()}')
+        else:
+            print(element)
+
+
+if __name__ == "__main__":
+    link_check(
+        r"O:\Lord of the Mods\_LIBRARY\BfME2-clean\!BfME2\data\ini\object\neutral\barrowwight.ini"
+    )
